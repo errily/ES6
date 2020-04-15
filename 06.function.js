3219115611d4c20cbb45052d86f2a999dfc45b71
errily <erril.98@gmail.com>
2020-04-15 22:57:24 +0700

    Create 06.function.js

---
+++ 06.function.js
@@ -0,0 +1,33 @@
+/*const upperizedNames1 = ["Erril", "Dhio", "Cemong"]
+    .map(function(name) {
+        return name.toUpperCase();
+    });
+
+console.log(upperizedNames1); 
+
+console.log('\n');
+
+const upperizedNames = ["Erril", "Dhio", "Cemong"]
+    .map(name => name.toUpperCase());
+
+console.log(...upperizedNames);
+
+//map memanggil nilai2 function
+
+//reguler vs arrow
+
+//function declaration
+function sayHello1(greet){
+    console.log(`${greet}!`);
+}
+
+// function expression
+const sayName1 = function (name) {
+    console.log(`Nama Saya ${name}`)
+}
+
+const sayHello = greet =>console.log(`${greet}`);
+const sayName = name => console.log(`Nama saya ${name}`);*/
+
+["cireng","cemong","tukung"]
+.forEach(name => console.log(`Nama kucing saya ${name}`));
