a6a21f14ed169f74f918ae05bdf854105745be3b
errily <erril.98@gmail.com>
2020-04-15 22:56:46 +0700

    Create 02.templateliteral.js

---
+++ 02.templateliteral.js
@@ -0,0 +1,12 @@
+const nama = "Afgan";
+const umurr = "30";
+const pekerjaan = "penyanyi";
+
+console.log("Nama "+ nama
++ "umur" +umurr + "pekerjaan " +
+pekerjaan)
+
+//literal
+console.log(`Nama ${nama}
+Umur ${umurr}
+Pekerjaan ${pekerjaan}`);
