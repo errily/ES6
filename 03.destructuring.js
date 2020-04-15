e3846c85ee16e9bd0a04882d8f399d9b1b7e4712
errily <erril.98@gmail.com>
2020-04-15 22:56:51 +0700

    Create 03.destructuring.js

---
+++ 03.destructuring.js
@@ -0,0 +1,10 @@
+const data = {
+	nama : 'erril',
+	umur : '21',
+	pekerjaan : 'mahasiswa'
+};
+
+const {nama, umur, pekerjaan} = data;
+console.log( `Nama ${nama}
+umur ${umur}
+pekerjaan ${pekerjaan} `);
